9b0a417558bf0f8ecd53c6d9fdb9f3829565d9d3
Ville Lampiselkä <148494501+pondbackjunior@users.noreply.github.com>
2024-04-20 23:20:02 +0300

    Update schema

--- schema.sql
+++ schema.sql
@@ -22,16 +22,33 @@ CREATE TABLE restaurants_ratings(
     sent_at TIMESTAMP
 );
 
-CREATE TABLE groups(
+CREATE TABLE categories(
     id SERIAL PRIMARY KEY,
     name TEXT UNIQUE
 );
 
-CREATE TABLE restaurants_groups(
+CREATE TABLE restaurants_categories(
     id SERIAL PRIMARY KEY,
     restaurant_id INTEGER REFERENCES restaurants,
-    group_id INTEGER REFERENCES groups
+    category_id INTEGER REFERENCES categories
 );
+
+CREATE TABLE types(
+    id SERIAL PRIMARY KEY,
+    name TEXT UNIQUE
+);
+
+CREATE TABLE restaurants_types(
+    id SERIAL PRIMARY KEY,
+    restaurant_id INTEGER REFERENCES restaurants,
+    type_id INTEGER REFERENCES types
+);
+
+INSERT INTO categories (name) VALUES ('Hampurilaiset'), ('Salaatti'), ('Jälkiruoka'), ('Kasvisruoka'), ('Amerikkalainen'), ('Pizza'),
+('Vegaani'), ('Jäätelö'), ('Katuruoka'), ('Voileivät'), ('Kebab'), ('Japanilainen'), ('Nuudelit'), ('Aamupala'), ('Terveellinen'),
+('Keitto'), ('Sushi'), ('Välimerellinen'), ('Italialainen'), ('Meksikolainen'), ('Kulhoruuat'), ('Nepalilainen'), ('Wings');
+
+INSERT INTO types (name) VALUES ('Hieno ruokailu'), ('Rento ruokailu'), ('Perhetyyli'), ('Buffet'), ('Kahvila'), ('Pikaruoka'), ('Ruoka-auto/koju');
 
 INSERT INTO restaurants (name, description, opening_time, closing_time)
 VALUES ('McDonald''s Helsinki Kamppi', 'Pitkäikäinen pikaruokaketju, joka tunnetaan hampurilaisista ja ranskalaisista.', '00:00:00', '23:59:00');
